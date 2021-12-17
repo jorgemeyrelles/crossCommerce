29d9e3fc25963d2eb9774812b48cf0fbb21d402b
jorgemeyrelles <jotaengpuc@gmail.com>
2021-12-17 18:30:38 -0300

    fix: route notfound

--- cross-project-front/src/App.js
+++ cross-project-front/src/App.js
@@ -5,6 +5,7 @@ import Numbers from './pages/Random';
 import SelectedNum from './pages/Selected';
 // import { getData } from './services/api';
 import './App.css';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route exact path="/" element= { <Home /> } />
       <Route exact path="/random/:id" element={ <Numbers /> } />
       <Route exact path="/selected/:id" element={ <SelectedNum /> } />
+      <Route path="*" element={ <NotFound /> } />
     </Routes>
   );
 }
--- cross-project-front/src/components/NotFound.js
+++ cross-project-front/src/components/NotFound.js
@@ -1,26 +1,31 @@
 import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router';
 import { getData } from '../services/api';
 
-// import { Container } from './styles';
-
 function NotFound() {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(false);
+  const navigate = useNavigate();
   const URL = 'http://localhost:3000/random';
   const path = window.location.pathname.split('/');
-
+  
   useEffect(() => {
     const receivingData = async () => {
       const numbers = await getData(`${URL}/${path[2]}`);
       if (numbers.length === 0) return setData(['[]']);
       setData(numbers);
+      console.log(numbers);
       return numbers;
     };
     receivingData();
     // setIsLoading(true);
-    console.log(data);
   }, []);
+
   return (
-    <di>{data}</di>
+    <>
+      <di>Not Found</di>
+      <div>404</div>
+      <button type='button' onClick={ () => navigate('/') }>Go back Home</button>
+    </>
   );
 }
 
